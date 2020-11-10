a7930bd2195af46b285ca789a23258893b0c0bc8
ledoen_local <ledoen@yikeke.com>
2020-11-10 07:41:24 -0800

    success use interruput: turn off led in main_loop, turn on in key_handler

--- src/int_key.s
+++ src/int_key.s
@@ -48,18 +48,19 @@ key_init:
 	orr r1, r1, #(0x1<<2)
 	str r1, [r0]
 	
+	@开启IRQ中断
+	mrc p15, 4, r0, c15, c0, 0		@获取GIC寄存器基地址
+	ldr r1, =0xffff0000
+	and r0, r0, r1
+	add r7, r0, #0x1000				@GICD寄存器基地址,存入r7
+	add r7, r7, #0x10c				@GICC_DENABLER寄存器地址
+	ldr r1, =0x4
+	str r1, [r7]
+	
 	mov pc, lr
 
 .global system_irq_handler
 system_irq_handler:
-	ldr r0, =0x0209c000		/*打开led*/
-	ldr r1, [r0]
-	ldr r2, =0x1
-	lsl r2, #9
-	bic r1, r1, r2
-	str r1, [r0]
-
-
 	ldr r1, =0x62			/*GPIO1_0-15对应的中断编号为98*/
 	cmp r0, r1
 	movne pc, lr			/*如果不等，不处理返回
--- src/main.s
+++ src/main.s
@@ -51,29 +51,39 @@ shutdown_int:
 	bl led_init
 	bl key_init
 	
+	
 	bl uart_newline
-	ldr r0, =0x209c00c
+
+	ldr r0, =0x0
+	mrc p15, 0, r0, c12, c0, 0
+	mov r6, r0
+	ldr r0, =0x90010000
+	str r6, [r0]
+	
 	bl uart_pri_r0
 	bl uart_newline
-	ldr r0, =0x209c014
-	bl uart_pri_r0
-	bl uart_newline
-	ldr r0, =0x209c018
-	bl uart_pri_r0
-	bl uart_newline
-	ldr r0, =0x209c01c
-	bl uart_pri_r0
-	bl uart_newline
+
 	
 	bl led_up
 
 
 main_loop:
-@	ldr r0, =0x0209c000	@持续点亮led灯，在中断函数中关闭led灯
-@	ldr r1, [r0]
-@	ldr r2, =0x1
-@	lsl r2, #9
-@	bic r1, r1, r2
-@	str r1, [r0]
+	ldr r0, =0x0209c000		/*关闭led*/
+	ldr r1, [r0]
+	ldr r2, =0x1
+	lsl r2, #9
+	orr r1, r1, r2
+	str r1, [r0]
+
+	ldr r0, =0x209c018
+	ldr r1, [r0]
+	and r1, r1, #0x4
+	lsr r1, #2
+	cmp r1, #0x1	@判断中断标志位是否为1
+	bne main_loop
+	bl uart_newline
+	ldr r0, =0x209c018
+	bl uart_pri_r0
+	bl uart_newline
 	b main_loop
 	
